33c7f3315b74b8713f772e84eae38adb44b1b5be
Pon saravana pandian B <pandiansaravana497@gmail.com>
2026-05-28 11:44:18 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// CAMU Timetable - Main JavaScript
+console.log('%cCAMU Timetable Loaded Successfully', 'color: #1976d2; font-weight: bold;');
+
+document.addEventListener('DOMContentLoaded', function() {
+    // You can add more interactivity here
+});
